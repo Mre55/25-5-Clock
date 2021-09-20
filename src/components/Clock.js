5ba842504449eee9b7499b0d954c01f8f4f7ce96
mre55 <misganawmihreteab>
2021-09-20 20:33:22 +0300

    add all files

---
+++ src/components/Clock.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import './Clock.css'
+
+class Clock extends React.Component {
+
+    render() {
+        return (
+            <h1>Me</h1>
+        )
+    }
+
+}
+
+export default Clock;
+
